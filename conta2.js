7e5ca07c2f10dc0d28eedbc061d10705fc943d43
Nathidombrowskii <nathielly.prado@escola.pr.gov.br>
2026-07-29 09:38:27 -0300

    Create conta2.js

---
+++ conta2.js
@@ -0,0 +1,13 @@
+function criarConta(titular,saldoInicial) {
+    let saldo = saldoInicial;
+
+    return {
+        titular,
+        depositar(valor) {
+            if (valor <= 0) {
+                console.log("Valor de depósito inválido");
+                return;
+            }
+        }
+    }
+}
