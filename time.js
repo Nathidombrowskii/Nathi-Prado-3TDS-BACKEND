7b39b7633dd98155f19c7d0f368f1e8a77db771d
Nathidombrowskii <nathielly.prado@escola.pr.gov.br>
2026-08-12 12:44:32 +0000

    versao1

---
+++ time.js
@@ -0,0 +1,14 @@
+class time {
+    constructor (jogador, tecnicos, cartoes, gol) {
+        this.jogador = jogador;
+        this.tecnicos = tecnicos;
+        this.cartoes = cartoes;
+        this.gol = gol;
+    apresentar () {
+        console.log (O ${this.jogador} fez ${this.gol});
+}
+    class timeFutebol extends time {
+        constructor (jogador, tecnicos, cartoes, gol, nomeTime)
+    }
+    }
+}
